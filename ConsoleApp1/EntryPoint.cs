0f313af4d047d463826cd43be003725462b5b38a
fmchora <fmchora@gmail.com>
2018-05-30 15:42:44 -0600

    spacecing added

--- ConsoleApp1/EntryPoint.cs
+++ ConsoleApp1/EntryPoint.cs
@@ -24,7 +24,7 @@ namespace AutotestFramework
   
         [SetUp]
         public void initialize()
-        {
+        { 
             NavigateTo.LoginFormThroughtTheMenuFirefox(driver);
         }
 
