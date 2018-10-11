9e0f1311581174de103672e29277e053e5dab8e4
fmchora <fmchora@gmail.com>
2018-10-11 14:12:34 -0600

    Added batch dictionary

--- AutoFramework/ExcelClass.cs
+++ AutoFramework/ExcelClass.cs
@@ -43,6 +43,29 @@ namespace AutoFramework
             return count;
         }
 
+        public Dictionary<string, string> returnDictionary()
+        {
+            Dictionary<string, string> dictionaryBatch = new Dictionary<string, string>()
+            {
+            { "DUNS Number", " " },                          { "Organization Name *", "ZQXKXRANNSFMXAG" },{ "Country Code *", "US" },
+            { "Address 1", " " },                            { "Address 2", " " },                        { "City", "Idaho" },
+            { "State/Province Code", "ID" },                 { "Postal Code", "" },                       { "Category *", "SUP" },
+            { "Contract Amount", "1000" },                   { "Internal Department", "FBI" },            { "Subsidiary/Parent", "Parent" },
+            { "Branch/Division *", "Internal Department 1" },{ "Contract Expiration Date", "7/21/2017" }, { "Start Date of Relationship", "7/21/2017" },
+            { "State Owned Entity", "No" },                  { "Ownership by a Public Official", "No" },  { "Interacts with Government Entities", "No" },
+            { "Third Party Payment Type", "Visa" },          { "Financial Risk", "No" },                  { "IT Security Risk", "No" },
+            { "Tax ID", "" },                                { "Business Registration Number", "" },      { "ID Number", "" },
+            { "Billed To", "" },                             { "Approval Status *", "" },                 { "Contact Company Name", "ZQXKXRANNSFMXAG" },
+            { "Contact Title", "" },                         { "Contact First Name", "" },                { "Contact Middle Name", "" },
+            { "Contact Last Name", "" },                     { "Contact Phone", "" },                     { "Contact Email", "" },
+            { "Contact Country Code", "US" },                { "Contact Address 1", "" },                 { "Contact Address 2", "" },
+            { "Contact City", "" },                          { "Contact State/Province Code", "ID" },     { "Contact Postal Code", "" },
+            { "Contact Language Code", "" },                 { "Third Party ID", "" },                    { "Owner Email *", "threepqa+level1@gmail.com" },
+            { "Approver Email", "" },                       {"Status", "" }
+            };
+            return dictionaryBatch;
+        }
+
         public void close()
         {
             workbook.Save();
